de4e3baae3f962f1d83a9991f0b25c3558f1c026
Aditya Sinha <adityasinha361@gmail.com>
2023-01-16 02:35:49 +0530

    feat: find longest string from an array

---
+++ question_4.js
@@ -0,0 +1,35 @@
+// Q4. Write a JavaScript function (both regular and arrow) which takes an array of string as parameter to find the longest string from a given array (read about string methods)
+
+const str = ['iphone', 'macbook', 'airpods', 'ipad']
+function longestString(str){
+    let maxLen = 0;
+    let longestWord;
+    for(let i = 0; i<str.length; i++){
+        let len = str[i].length;
+        
+        if(maxLen < len){
+            maxLen = len;
+            longestWord = str[i];
+        }
+    }
+    return longestWord;
+}
+
+const longestStringArrow = (str) => {
+    let maxLen = 0;
+    let longestWord;
+    for(let i = 0; i<str.length; i++){
+        let len = str[i].length;
+        
+        if(maxLen < len){
+            maxLen = len;
+            longestWord = str[i];
+        }
+    }
+    return longestWord;
+} 
+
+console.log(longestString(str));
+console.log(longestStringArrow(str));
+
+module.exports = {longestString, longestStringArrow}
