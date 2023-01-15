7d3531bbf03483c2a71db44836b9ee7a65e6f048
Aditya Sinha <adityasinha361@gmail.com>
2023-01-16 02:34:26 +0530

    feat: change all capitalization in a word

---
+++ question_3.js
@@ -0,0 +1,29 @@
+// Q3. Write a JavaScript function (both regular and arrow) which takes a string as parameter, to change the capitalization of all letters in a given string and returns the result and then print it.
+const str = 'AditYa'
+
+function capitalization(str){
+    let strLen = str.length;
+    let newStr = '';
+    for(let i = 0; i<strLen; i++){
+        let char = str[i];
+        if(char != char.toUpperCase()) newStr+=char.toUpperCase();
+        else newStr+=char.toLowerCase();
+    }
+    return newStr;
+}
+
+const capitalizationArrow = (str) =>{
+    let strLen = str.length;
+    let newStr = '';
+    for(let i = 0; i<strLen; i++){
+        let char = str[i];
+        if(char != char.toUpperCase()) newStr+=char.toUpperCase();
+        else newStr+=char.toLowerCase();
+    }
+    return newStr;
+}
+
+console.log(capitalization(str));
+console.log(capitalizationArrow(str));
+
+module.exports = {capitalization, capitalizationArrow} 
